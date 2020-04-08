cfd8ce9933940ce3a0ce345561656e9453d64e39
James Oquendo <james.oquendo@gmail.com>
2020-04-07 22:43:14 -0700

    Apply the rules to the Game of Life

--- udemy/build-the-game-of-life/code.js
+++ udemy/build-the-game-of-life/code.js
@@ -80,6 +80,7 @@ function cellClickHandler() {
     var rowcol = this.id.split('_');
     var row = rowcol[0];
     var col = rowcol[1];
+
     var classes = this.getAttribute('class');
 
     if (classes.indexOf('live') > -1) {
@@ -109,7 +110,86 @@ function setupControlButtons() {
 function play() {
 
     console.log('play the game');
+    computeNextGen();
 
+}
+
+function computeNextGen() {
+
+    for (var i = 0; i < rows; i++) {
+
+        for (var j = 0; j < cols; j++) {
+
+            applyRules(i, j);
+
+        }
+
+    }
+
+}
+
+function applyRules(row, col) {
+
+    var numNeighbors = countNeighbors(row, col);
+
+    if (grid[row][col] == 1) {
+
+        if (numNeighbors < 2) {
+
+            nextGrid[row][col] = 0;
+
+        } else if (numNeighbors == 2 || numNeighbors == 3) {
+
+            nextGrid[row][col] = 1;
+
+        } else if (numNeighbors > 3) {
+
+            nextGrid[row][col] = 0;
+
+        }
+
+    } else if (grid[row][col] == 0) {
+
+        if (numNeighbors == 3) {
+
+            nextGrid[row][col] = 1;
+
+        }
+
+    }
+
+}
+
+function countNeighbors(row, col) {
+
+    var count = 0;
+
+    if (row-1 >= 0) {
+        if (grid[row-1][col] == 1) count++;
+    }
+    if (row-1 >= 0 && col-1 >= 0) {
+        if (grid[row-1][col-1] == 1) count++;
+    }
+    if (row-1 >= 0 && col+1 < cols) {
+        if (grid[row-1][col+1] == 1) count++;
+    }
+    if (col-1 >= 0) {
+        if (grid[row][col-1] == 1) count++;
+    }
+    if (col+1 < cols) {
+        if (grid[row][col+1] == 1) count++;
+    }
+    if (row+1 < rows) {
+        if (grid[row+1][col] == 1) count++;
+    }
+    if (row+1 < rows && col-1 >= 0) {
+        if (grid[row+1][col-1] == 1) count++;
+    }
+    if (row+1 < rows && col+1 <= cols) {
+        if (grid[row+1][col+1] == 1) count++;
+    }
+
+    return count;
 }
 
 function startButtonClickHandler() {
